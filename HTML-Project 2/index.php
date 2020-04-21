db37c9daa63f1ee018135b4679ffc111cd52d01c
yashagarwal314 <57199043+yashagarwal314@users.noreply.github.com>
2020-04-20 20:44:28 -0700

    Files from replit

---
+++ HTML-Project 2/index.php
@@ -0,0 +1,25 @@
+ <html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links: Yash Agarwal Period 7</p>'; ?> 
+    <ul>
+      <li><a href="Agarwal_index.html">index.html</a></li></br>
+      <!-- This is my main page where all the links I have are present-->
+      <li><a href="Agarwal_AboutMe.html">About Me</a></li></br>
+      
+      <li><a href="Agarwal_Projects.html">Project</a></li></br>
+      
+      <li><a href="Agarwal_Resume.html">Resume</a></li></br>
+   
+      <li><a href="Agarwal_ContactMe.html">Contact Me</a></li></br>
+
+       <li><a href="Agarwal_References.html">My References</a></li></br>
+  
+  
+   <ul>
+
+ </body>
+
+</html>
